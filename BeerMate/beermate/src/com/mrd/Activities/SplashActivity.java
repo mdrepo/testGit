ac92ee9dd96ef005414091537c3b01af496b5f04
dubemayur <dubemayur@gmail.com>
2014-05-08 22:17:35 +0530

    initial commit
    initial commit to test project

---
+++ BeerMate/beermate/src/com/mrd/Activities/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.mrd.Activities;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import com.mrd.beermateapp.*;;
+
+public class SplashActivity extends Activity {
+
+	Button colorSpace;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.select_dialog_item plash_activity);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+}
